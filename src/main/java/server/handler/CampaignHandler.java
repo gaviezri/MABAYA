80d2004838f555e766cfb45f7f4e8f2f282fe848
Gal Aviezri <g.aviezri@gmail.com>
2023-06-07 17:06:18 +0300

    first commit

--- src/main/java/server/handler/CampaignHandler.java
+++ src/main/java/server/handler/CampaignHandler.java
@@ -2,13 +2,28 @@ package server.handler;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import db.DBManager;
 
 import java.io.IOException;
 
 public class CampaignHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
+        if (!method.equals("POST")){
+            System.out.println("Invalid request method");
+            exchange.sendResponseHeaders(405, -1);
+            return;
+        }
+        try {
+            String JSONResult = DBManager.insertNewCampaign(exchange.getRequestBody());
+            exchange.sendResponseHeaders(200, JSONResult.length());
+            exchange.getResponseBody().write(JSONResult.getBytes());
 
-
+        } catch (IllegalArgumentException e) {
+            String response = "Invalid request body - most likely illegal product ID in campaign.";
+            exchange.sendResponseHeaders(405, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+        }
     }
 }
